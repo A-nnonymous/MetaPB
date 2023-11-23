73f6f4090884f9a8ae55a144eb0756800bd9c6ec
Pan Zhaowu <panzhaowu21s@ict.ac.cn>
2023-11-23 06:54:35 +0000

    Add currently utilized repos

---
+++ include/utils/dpuCompile.hpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <map>
+#include <vector>
+
+/// @brief A stateless dpu compile proxy.
+class DPUCompileProxy{
+public:
+  struct compileArgs{
+    int optimizationLevel = 2;                // Set -O2 as default.
+    int taskletNum =12;                       // Aligned with SimplePIM
+    std::string outputFilePath {"./foo"};     // A meanless placeholder.
+    std::vector<std::string> postfixes = {};  // Postfixes that able to be customized by user.
+  };
+  void compileWith(compileArgs &args)const noexcept;
+
+private:
+  enum compileToken {
+    compilerBin,        // DPU compiler's binary name, set to constant
+    optimizationLevel,  
+    taskletNum,         // NR_TASKLET by convention, in SimplePIM fixed to constant 12. 
+    includeDir,         // Explicit specified header directories.
+    outputFilePath,     // Single output binary path.
+  };
+  static inline std::map<compileToken, std::string> tokenLUT = {
+    {compilerBin, "dpu-upmem-dpurte-clang"},
+    {optimizationLevel, " -O"},
+    {taskletNum, " -DNR_TASKLETS="},
+    {outputFilePath, " -o"}
+  };
+  std::string parse(compileArgs &args)const noexcept;
+}; // DPUCompileProxy
+
+
